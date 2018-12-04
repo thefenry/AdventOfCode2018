c7c3f6592b8f31662284c4f2c2da481acf419237
Ivan Sued <thefenry@gmail.com>
2018-12-04 13:13:38 -0800

    Solving Part One

---
+++ day-one/Parts/DayOnePartService.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace day_one.Parts
+{
+    public class DayOnePartService
+    {
+        public int GetFinalFrequency()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Inputs\FrequencyInputs.txt");
+            string[] inputs = File.ReadAllLines(path);
+
+            int valueFrequency = 0;
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                var validNumber = int.TryParse(inputs[i], out int value);
+                if (validNumber)
+                {
+                    valueFrequency += value;
+                }
+            }
+
+            return valueFrequency;
+        }
+    }
+}
--- day-one/Program.cs
+++ day-one/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using day_one.Parts;
 
 namespace day_one
 {
     class Program
     {
+        private static DayOnePartService _service;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _service = new DayOnePartService();
+            
+            DisplayFinalFrequency();
+        }
+
+        private static void DisplayFinalFrequency()
+        {
+
+            int finalFrequency = _service.GetFinalFrequency();
+
+            Console.WriteLine($"Final Frequency: {finalFrequency}");
+
+            Console.ReadLine();
         }
     }
 }
