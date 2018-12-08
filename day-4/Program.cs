9de1142c8e725f019b75abeb8657eb47212c4205
Ivan Sued <thefenry@gmail.com>
2018-12-08 15:34:04 -0800

    Solving part 0ne with brute force

--- day-4/Program.cs
+++ day-4/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 
 namespace day_4
-{     
+{
     class BasicActivity
     {
         public DateTime ActivityDate { get; set; }
@@ -13,29 +13,134 @@ namespace day_4
         public string ActivitySummary { get; set; }
     }
 
-    //class GuardActivity
-    //{
-    //    public string Id { get; set; }
+    class GuardActivityDetail
+    {
+        public string GuardId { get; set; }
 
-    //    public int? MinuteAsleep { get; set; }
+        public DateTime ShiftStartDate { get; set; }
 
-    //    public int? MinuteWakingUp { get; set; }
+        public List<Activity> Activities { get; set; } = new List<Activity>();
 
-    //    public bool WakesUp { get; set; } = false;
+        public int TotalTimeAsleep { get; set; }
+    }
 
-    //    public bool FallsAsleep { get; set; } = false;
+    public class Activity
+    {
+        public Activity(int minute, string type, DateTime activityDate)
+        {
+            this.ActivityType = type;
+            MinuteActivity = minute;
+            ActivityDate = activityDate;
+        }
 
-    //    public bool BeginsShift { get; set; } = false;
+        public int MinuteActivity { get; set; }
 
-    //    public DateTime ActivityDate { get; set; }
-    //}
+        public string ActivityType { get; set; }
+
+        public DateTime ActivityDate { get; set; }
+    }
 
     class Program
     {
+        private static bool foundNumber;
+
         static void Main(string[] args)
         {
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"GuardSchedules.txt");
             string[] inputs = File.ReadAllLines(path);
+
+            List<BasicActivity> guardActivities = ParseBasicActivty(inputs);
+
+            List<BasicActivity> sortedGuardActivities = guardActivities.OrderBy(c => c.ActivityDate).ToList();
+
+            List<GuardActivityDetail> guardActivityDetails = GetGuardsShiftActivitySchedule(sortedGuardActivities);
+
+            List<IGrouping<string, GuardActivityDetail>> groupedGuards = guardActivityDetails.GroupBy(c => c.GuardId).ToList();
+
+            List<GuardActivityDetail> guardWithMostAsleepTime = GetGuardWithMostAsleepTime(groupedGuards);
+
+            int guardId = int.Parse(guardWithMostAsleepTime.First().GuardId.Split('#', StringSplitOptions.RemoveEmptyEntries)[0]);
+
+            List<Activity> activities = guardWithMostAsleepTime.SelectMany(c => c.Activities).ToList();
+
+            for (int i = 0; i < activities.Count; i++)
+            {
+                Activity activity = activities[i];
+
+                for (int j = 0; j < activities.Count; j++)
+                {
+                    Activity activityToCheckAgainst = activities[j];
+                    if (activityToCheckAgainst.ActivityDate.Day != activity.ActivityDate.Day && activity.ActivityType == "falls")
+                    {
+                        if (activity.MinuteActivity <= activityToCheckAgainst.MinuteActivity)
+                        {
+                            if (activities[i + 1].MinuteActivity >= activityToCheckAgainst.MinuteActivity)
+                            {
+                                int range2Min = activityToCheckAgainst.MinuteActivity;
+                               
+                                // answer 67558
+                                Console.WriteLine(range2Min * guardId);
+                                foundNumber = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (foundNumber)
+                {
+                    break;
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static List<GuardActivityDetail> GetGuardWithMostAsleepTime(List<IGrouping<string, GuardActivityDetail>> groupedGuards)
+        {
+            string guardId = string.Empty;
+            int guardTotalTimeAsleep = 0;
+            List<GuardActivityDetail> guardInfoWithMostSleepTime = new List<GuardActivityDetail>();
+
+            for (int i = 0; i < groupedGuards.Count; i++)
+            {
+                IGrouping<string, GuardActivityDetail> guard = groupedGuards[i];
+
+                int totalTimeAsleep = 0;
+
+                List<GuardActivityDetail> guardActivitiesList = guard.ToList();
+
+                for (int j = 0; j < guardActivitiesList.Count; j++)
+                {
+                    totalTimeAsleep += guardActivitiesList[j].TotalTimeAsleep;
+                }
+
+                if (string.IsNullOrEmpty(guardId))
+                {
+                    guardId = guard.Key;
+                    guardTotalTimeAsleep = totalTimeAsleep;
+                }
+                else
+                {
+                    if (guardTotalTimeAsleep < totalTimeAsleep)
+                    {
+                        guardId = guard.Key;
+                        guardTotalTimeAsleep = totalTimeAsleep;
+                    }
+                }
+
+                if (guard.Key == guardId)
+                {
+                    guardInfoWithMostSleepTime = guardActivitiesList;
+                }
+            }
+
+            return guardInfoWithMostSleepTime;
+        }
+
+        private static List<BasicActivity> ParseBasicActivty(string[] inputs)
+        {
             char[] timeSplitter = new char[] { '[', ']' };
 
             List<BasicActivity> guardActivities = new List<BasicActivity>();
@@ -49,54 +154,53 @@ namespace day_4
                 BasicActivity guardInfo = new BasicActivity
                 {
                     ActivityDate = DateTime.Parse(content[0]),
-                    ActivitySummary = guardInfoInput[1]
+                    ActivitySummary = content[1]
                 };
-
-
-                //if (guardInfoInput[0] == "Guard")
-                //{
-                //    guardInfo.Id = guardInfoInput[1];
-                //    guardInfo.BeginsShift = true;
-                //}
-                //else
-                //{
-                //    if (guardInfoInput[0] == "falls")
-                //    {
-                //        guardInfo.FallsAsleep = true;
-                //        guardInfo.MinuteAsleep = guardInfo.ActivityDate.Minute;
-                //    }
-                //    else
-                //    {
-                //        guardInfo.WakesUp = true;
-                //        guardInfo.MinuteWakingUp = guardInfo.ActivityDate.Minute;
-                //    }
-                //}
 
                 guardActivities.Add(guardInfo);
             }
 
-            var groupedGuardActivities = guardActivities.OrderBy(c => c.ActivityDate).GroupBy(c =>c.ActivityDate).ToList();
+            return guardActivities;
+        }
 
-            //var guardSleepSchedule = new List<GuardActivity>();
+        private static List<GuardActivityDetail> GetGuardsShiftActivitySchedule(List<BasicActivity> sortedGuardActivities)
+        {
+            List<GuardActivityDetail> guardActivityDetails = new List<GuardActivityDetail>();
 
-            //foreach (var activity in guardActivities)
-            //{
+            foreach (BasicActivity guardActivity in sortedGuardActivities)
+            {
+                string[] guardActivityFragment = guardActivity.ActivitySummary.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
+                if (guardActivityFragment[0] == "Guard")
+                {
+                    GuardActivityDetail guardDetail = new GuardActivityDetail
+                    {
+                        GuardId = guardActivityFragment[1],
+                        ShiftStartDate = guardActivity.ActivityDate
+                    };
 
-            //    if (!string.IsNullOrWhiteSpace(activity.Id))
-            //    {
-            //        var guardActivity = new GuardActivity
-            //        {
-            //            Id = activity.Id
-            //        };
+                    guardActivityDetails.Add(guardDetail);
+                }
+                else
+                {
+                    if (guardActivityFragment[0] == "falls")
+                    {
+                        GuardActivityDetail guardDetail = guardActivityDetails.Last();
+                        guardDetail.Activities.Add(new Activity(guardActivity.ActivityDate.Minute, "falls", guardActivity.ActivityDate));
 
+                    }
+                    else
+                    {
+                        GuardActivityDetail guardDetail = guardActivityDetails.Last();
+                        guardDetail.Activities.Add(new Activity(guardActivity.ActivityDate.Minute, "wakes", guardActivity.ActivityDate));
 
-            //    }
+                        int activitiesCount = guardDetail.Activities.Count;
+                        guardDetail.TotalTimeAsleep += guardDetail.Activities[activitiesCount - 1].MinuteActivity - guardDetail.Activities[activitiesCount - 2].MinuteActivity;
+                    }
+                }
+            }
 
-            //    Console.WriteLine($"{activity.ActivityDate}-{activity.Id}-{activity.FallsAsleep}-{activity.WakesUp}");
-            //}
-
-            Console.ReadLine();
+            return guardActivityDetails;
         }
     }
 }
